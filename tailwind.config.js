4d2d5583fe5200eae3e2cf51215972f4e2b43b2b
bishtjaswant <jaswantbishtuk1995@gmail.com>
2023-11-10 21:22:42 +0530

    landing page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+         playFair:"'Playfair Display', serif",
+         lato:"'Lato', sans-serif"
+      },
+    },
+  },
+  plugins: [],
+}
